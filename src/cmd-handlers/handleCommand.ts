294653cde2d9faa8e70fbd0add68df08e26b4254
Yury_Viarenich <verenichyi@gmail.com>
2024-10-19 17:58:34 +0300

    feat: add enum with message types

--- src/cmd-handlers/handleCommand.ts
+++ src/cmd-handlers/handleCommand.ts
@@ -2,26 +2,13 @@ import { Duplex } from "node:stream";
 import { handleRegistration } from "./handleRegistration";
 import { Message, RegRequestData } from "../models/interfaces";
 import { MessageData } from "../models/types";
+import { MessageTypes } from "../enums/messageTypes";
 
 export const handleCommand = (ws: Duplex, message: Message<MessageData>): void => {
     const { type, data, id } = message;
     switch (type) {
-        case 'reg':
+        case MessageTypes.REG:
             handleRegistration(ws, data as RegRequestData, id);
             break;
-        // case 'create_room':
-        //     handleCreateRoom(ws, data, id);
-        //     break;
-        // case 'add_user_to_room':
-        //     handleAddUserToRoom(ws, data, id);
-        //     break;
-        // case 'add_ships':
-        //     handleAddShips(ws, data, id);
-        //     break;
-        // case 'attack':
-        //     handleAttack(ws, data, id);
-        //     break;
-        default:
-            ws.write(JSON.stringify({ type: 'error', data: JSON.stringify({ error: true, errorText: 'Unknown command' }), id }));
     }
 }
--- src/cmd-handlers/handleRegistration.ts
+++ src/cmd-handlers/handleRegistration.ts
@@ -1,19 +1,20 @@
 import { Duplex } from "node:stream";
 import { Player, RegRequestData } from "../models/interfaces";
 import { parseWsResponseMessage } from "../helpers";
+import { MessageTypes } from "../enums/messageTypes";
 
 const players: Player[] = [];
 export const handleRegistration = (ws: Duplex, data: RegRequestData, id: number): void => {
     const existingPlayer = players.find(player => player.name === data.name);
     if (existingPlayer) {
         if (existingPlayer.password === data.password) {
-            ws.write(parseWsResponseMessage({ type: 'reg', data: { name: data.name, index: existingPlayer.id, error: false, errorText: '' }, id }));
+            ws.write(parseWsResponseMessage({ type: MessageTypes.REG, data: { name: data.name, index: existingPlayer.id, error: false, errorText: '' }, id }));
         } else {
-            ws.write(parseWsResponseMessage({ type: 'reg', data: { name: data.name, index: existingPlayer.id, error: true, errorText: 'Incorrect password' }, id }));
+            ws.write(parseWsResponseMessage({ type: MessageTypes.REG, data: { name: data.name, index: existingPlayer.id, error: true, errorText: 'Incorrect password' }, id }));
         }
     } else {
         const newPlayer: Player = { name: data.name, password: data.password, id: players.length + 1, wins: 0 };
         players.push(newPlayer);
-        ws.write(parseWsResponseMessage({ type: 'reg', data: { name: data.name, index: newPlayer.id, error: false, errorText: '' }, id }));
+        ws.write(parseWsResponseMessage({ type: MessageTypes.REG, data: { name: data.name, index: newPlayer.id, error: false, errorText: '' }, id }));
     }
 }
