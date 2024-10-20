bb0d90b9464d290af8da259bfdf0edc9a1f38a71
Yury_Viarenich <verenichyi@gmail.com>
2024-10-20 11:12:32 +0300

    refactor: starting wss

--- index.ts
+++ index.ts
@@ -1,38 +1,72 @@
 import { createWebSocketStream, WebSocket, WebSocketServer } from 'ws';
 import { httpServer } from './src/http_server';
-import { parseWsRequestMessage } from "./src/helpers";
-import CommandHandler from "./src/command-handler";
-
+import { parseWsRequestMessage } from './src/wss/helpers';
+import CommandHandler from './src/wss/command-handler';
 
 const HTTP_PORT = 8181;
-httpServer.listen(HTTP_PORT, () => console.log(`Start static http server on the ${HTTP_PORT} port!`));
+httpServer.listen(HTTP_PORT, () =>
+    console.log(`Start static http server on the ${HTTP_PORT} port!`),
+);
 
-const WS_PORT = 3000;
-const wss = new WebSocketServer({ port: WS_PORT }, () => console.log(`WebSocket server start on the ${WS_PORT} port!`));
+class WSS {
+    private WS_PORT = 3000;
+    private wss: WebSocketServer;
+    private commandHandler: CommandHandler;
 
-const commandHandler = new CommandHandler();
-const handleMessage = async (ws: WebSocket, client: string) => {
-    const wsStream = createWebSocketStream(ws, { encoding: 'utf8', decodeStrings: false });
+    constructor() {
+        this.wss = new WebSocketServer({ port: this.WS_PORT });
+        this.commandHandler = new CommandHandler();
+    }
 
-    wsStream.on('data', async (message) => {
+    async handleMessage(ws: WebSocket, client: string) {
+        const wsStream = createWebSocketStream(ws, {
+            encoding: 'utf8',
+            decodeStrings: false,
+        });
+
+        wsStream.on('data', async (message) => {
+            try {
+                const request = parseWsRequestMessage(message);
+                console.log({ request });
+                this.commandHandler.handleCommand(ws, wsStream, request);
+            } catch (error) {
+                console.log(error);
+            }
+        });
+    };
+
+    async startWSS() {
         try {
-            const request = parseWsRequestMessage(message);
-            console.log({ request })
-            commandHandler.handleCommand(ws, wsStream, request);
+            this.wss
+                .on('connection', async (ws, req) => {
+                    const clientAddress = req.headers['origin'];
+                    console.log(`Client ${clientAddress} connected!`);
+
+                    await this.handleMessage(ws, clientAddress);
+
+                    ws.on('close', () => {
+                        ws.close();
+                        console.log(`Client ${clientAddress} disconnected!`);
+                    });
+                })
+                .on('listening', () => {
+                    console.log(`WebSocket server start on the ${this.WS_PORT} port!`);
+                })
+                .on('error', (err) => console.error(err))
+                .on('close', () => {
+                    this.wss.clients.forEach((client) => {
+                        if (client.readyState === WebSocket.OPEN) {
+                            client.close();
+                        }
+                    });
+
+                    console.log('WSS closed');
+                });
         } catch (error) {
-            ws.send(JSON.stringify({ type: 'error', data: { error: true, errorText: 'Invalid message format' }, id: 0 }));
+            console.error(error);
         }
-    });
-};
+    }
+}
 
-wss.on('connection', async (ws, req) => {
-    const clientAddress = req.headers['origin'];
-    console.log(`Client ${clientAddress} connected!`);
-
-    await handleMessage(ws, clientAddress);
-
-    ws.on('close', () => {
-        ws.close();
-        console.log(`Client ${clientAddress} disconnected!`);
-    });
-});
+const wss = new WSS();
+await wss.startWSS();
